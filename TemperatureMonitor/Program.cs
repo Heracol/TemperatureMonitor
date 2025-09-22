ed0d24fde2f088cc758ff09af3366a6e5b79bf15
Pranav <pranav.tambat@gmail.com>
2025-09-22 12:12:19 +0200

    nofity icon

--- TemperatureMonitor/Program.cs
+++ TemperatureMonitor/Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using LibreHardwareMonitor.Hardware;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using LibreHardwareMonitor.Hardware;
 
 namespace TemperatureMonitor
 {
@@ -16,13 +18,15 @@ namespace TemperatureMonitor
             IsCpuEnabled = true
         };
 
+        static NotifyIcon notifyIcon = new NotifyIcon();
+
         static void Main(string[] args)
         {
             StartProcess();
 
-            
+            notifyIcon.Visible = true;
 
-            Console.ReadLine();
+            Application.Run();
 
             computer.Close();
         }
@@ -31,7 +35,7 @@ namespace TemperatureMonitor
         {
             computer.Open();
 
-            new Timer(GetTemperature, null, 0, 1000);
+            new System.Threading.Timer(GetTemperature, null, 0, 1000);
         }
 
         public static void GetTemperature(object state)
@@ -43,7 +47,34 @@ namespace TemperatureMonitor
                 ISensor sensor = hardware.Sensors.First(s => s.Name.Equals("Core Average"));
 
                 Console.WriteLine($"{sensor.Name}, {sensor.SensorType}, {sensor.Value}");
+
+                if (sensor.Value != null)
+                {
+                    Icon icon = GetIcon((int)Math.Round((float)sensor.Value));
+                    notifyIcon.Icon = icon;
+                    DestroyIcon(icon.Handle);
+                }
             }
         }
+
+        public static Icon GetIcon(int value)
+        {
+            Bitmap bitmap = new Bitmap(32, 32);
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                Font font = new Font("Roboto", 16, FontStyle.Bold);
+                graphics.DrawString(value.ToString(), font, Brushes.White, new PointF(4, 4));
+            }
+
+            IntPtr Hicon = bitmap.GetHicon();
+
+            Icon newIcon = Icon.FromHandle(Hicon);
+
+            return newIcon;
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
     }
 }
