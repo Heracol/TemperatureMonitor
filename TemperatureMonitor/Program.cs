b836e0fd3ecbcc848158b5d6c8daf8ca5f48f18d
Pranav <pranav.tambat@gmail.com>
2025-09-22 11:34:48 +0200

    getting values

--- TemperatureMonitor/Program.cs
+++ TemperatureMonitor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibreHardwareMonitor.Hardware;
 
 namespace TemperatureMonitor
 {
@@ -10,6 +11,56 @@ namespace TemperatureMonitor
     {
         static void Main(string[] args)
         {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                Console.WriteLine("Hardware: {0}", hardware.Name);
+
+                foreach (IHardware subhardware in hardware.SubHardware)
+                {
+                    Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+
+                    foreach (ISensor sensor in subhardware.Sensors)
+                    {
+                        Console.WriteLine("\t\tSensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType);
+                    }
+                }
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    Console.WriteLine("\tSensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType);
+                }
+            }
+
+            computer.Close();
         }
+    }
+
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
     }
 }
